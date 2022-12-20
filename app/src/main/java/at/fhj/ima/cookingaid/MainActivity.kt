adaf43074dd3261d6aa998de37f4b9fdaf98aa2b
nico-loy <116094000+nico-loy@users.noreply.github.com>
2022-12-20 17:13:36 +0100

    User Interface v1

--- app/src/main/java/at/fhj/ima/cookingaid/MainActivity.kt
+++ app/src/main/java/at/fhj/ima/cookingaid/MainActivity.kt
@@ -1,11 +1,20 @@
 package at.fhj.ima.cookingaid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<ImageButton>(R.id.open_recipe_list).setOnClickListener {
+            val intent = Intent(this, RecipeListActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
