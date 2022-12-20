da4f1b77c2270042c9f387a52e15d2c45c69c3ce
nico-loy <116094000+nico-loy@users.noreply.github.com>
2022-12-20 16:00:42 +0100

    Initial commit

---
+++ app/src/main/java/at/fhj/ima/cookingaid/MainActivity.kt
@@ -0,0 +1,11 @@
+package at.fhj.ima.cookingaid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
