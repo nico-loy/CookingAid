20373bba6d3df739d64bca5ef86d2ddc478d5139
nico-loy <116094000+nico-loy@users.noreply.github.com>
2023-01-10 12:40:44 +0100

    Test

--- app/src/main/java/at/fhj/ima/cookingaid/RecipeListActivity.kt
+++ app/src/main/java/at/fhj/ima/cookingaid/RecipeListActivity.kt
@@ -7,5 +7,6 @@ class RecipeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_list)
+
     }
 }
