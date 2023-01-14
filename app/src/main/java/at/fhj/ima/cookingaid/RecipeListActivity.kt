946452cad44f399ed3025e250f1b3774f2389d64
nico-loy <116094000+nico-loy@users.noreply.github.com>
2023-01-14 15:25:02 +0100

    CardViews

--- app/src/main/java/at/fhj/ima/cookingaid/RecipeListActivity.kt
+++ app/src/main/java/at/fhj/ima/cookingaid/RecipeListActivity.kt
@@ -2,11 +2,22 @@ package at.fhj.ima.cookingaid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 class RecipeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recipe_list)
+    }
 
+    override fun onCreate(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+): View? {
+        val view = inflater.inflate(R.layout.activity_recipe_list, container, false)
+
+        return view
     }
 }
