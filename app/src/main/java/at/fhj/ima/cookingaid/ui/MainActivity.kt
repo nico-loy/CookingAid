9e4c560c4f49114fe50f6d26e78a6a196b3ad803
nico-loy <116094000+nico-loy@users.noreply.github.com>
2023-01-21 23:57:57 +0100

    Data Extraction Rules Issue

--- app/src/main/java/at/fhj/ima/cookingaid/ui/MainActivity.kt
+++ app/src/main/java/at/fhj/ima/cookingaid/ui/MainActivity.kt
@@ -4,18 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
-import at.fhj.ima.cookingaid.R
-import at.fhj.ima.cookingaid.RecipeListActivity
+import at.fh.mappdev.loggingviewsandactivity.R
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<ImageButton>(R.id.open_recipe_list).setOnClickListener {
+       /* findViewById<ImageButton>(R.id.open_recipe_list).setOnClickListener {
             val intent = Intent(this, RecipeListActivity::class.java)
             startActivity(intent)
         }
+
+        */
     }
 
 
